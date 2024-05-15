90dd99d4690fb957a76ff66202409c0c0a14c2c9
Gyan Gupta <gyandev082@gmail.com>
2024-05-15 15:51:47 +0530

    Establish association b/w event & attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,15 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :attendances, dependent: :destroy
+  has_many :attendees, through: :attendances, source: :attendee
   validates :name, presence: true
   validate :date_in_future
 
   private
 
   def date_in_future
-    if date.present? && date < Date.today
-      errors.add(:date, 'Must be in the future')
-    end
+    return unless date.present? && date < Date.today
+
+    errors.add(:date, 'Must be in the future')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
+  has_many :attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :attendances
 end
