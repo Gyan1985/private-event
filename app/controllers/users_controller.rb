84bc021fbedadfb068f4e18f1bb5ff426bda5867
Gyan Gupta <gyandev082@gmail.com>
2024-05-15 13:28:11 +0530

    Added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def hosted_events
-    @hosted_events = current_user.created_events.includes(:creator)
+    hosted_events = current_user.created_events.includes(:creator)
+    @hosted_events = hosted_events.page(params[:page])
   end
 end
