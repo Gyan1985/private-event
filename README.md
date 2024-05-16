5ce6287a24d3273ba191aec8d49a406f67fc389c
Gyan Gupta <gyandev082@gmail.com>
2024-05-16 13:36:17 +0530

    Update seed and readme

--- README.md
+++ README.md
@@ -5,20 +5,58 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+## Requirements
+- Ruby ~> 3.0.0
+- Rails ~> 7.0.8
+- PostgreSQL
+- Other dependencies are listed in the `Gemfile`.
 
-* System dependencies
+## Installation
 
-* Configuration
+#1. Clone the repository:
 
-* Database creation
+```bash
+git clone https://github.com/Gyan1985/private-event.git
+cd private-event
+```
 
-* Database initialization
+#2. Install dependencies:
 
-* How to run the test suite
+```bash
+bundle install
+```
 
-* Services (job queues, cache servers, search engines, etc.)
+#3. Set up the database:
 
-* Deployment instructions
+```bash
+rails db:create
+rails db:migrate
+```
 
-* ...
+#4. Seed the database with initial data:
+
+```bash
+rails db:seed
+```
+
+## Running the Application
+
+To start the server, run:
+
+```bash
+rails server
+```
+
+This will start the server at [http://localhost:3000](http://localhost:3000).
+
+## Testing
+
+Run the tests using RSpec:
+
+```bash
+rspec
+```
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any changes or enhancements.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+# Create Users
+
+event_host = User.find_or_create_by!(
+  email: 'event_host@example.com'
+) do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
+
+puts "Event host created: #{event_host.email}"
+
+event_attendee = User.find_or_create_by!(
+  email: 'event_attendee@example.com'
+) do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
+
+puts "Event attendee created: #{event_attendee.email}"
+
+# Create Events
+future_event = Event.find_or_create_by(name: 'Event 1', date: Date.today + 1.day, creator: event_host)
+
+puts "Event for future created: #{future_event.name}"
+
+past_event = Event.find_or_initialize_by(name: 'Event 2', date: Date.today - 1.day, creator: event_host)
+past_event.save(validate: false)
+
+puts "Event for past created: #{past_event.name}"
+
+attendee_hosted_event = Event.find_or_create_by(name: 'Event 3', date: Date.today + 1.day, creator: event_attendee)
+
+puts "Event hosted by attendee created: #{attendee_hosted_event.name}"
+
+# Create some attendances
+Attendance.find_or_create_by(attendee: event_attendee, event: future_event)
+Attendance.find_or_create_by(attendee: event_attendee, event: past_event)
